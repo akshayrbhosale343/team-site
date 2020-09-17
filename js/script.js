ca63d33dbeb6c46e88abd4a38225b73a31759da0
rhasan11 <AKSHAY@DESKTOP.com>
2020-09-17 20:39:55 +0530

    added video for testing

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
 function copyToClipboard() {
     document.getElementById("contact-mail").select()
-    document.getElementById("contact-mail").setSelectionRange(0,99999)
+    document.getElementById("contact-mail").setSelectionRange(0, 99999)
     document.execCommand("copy")
-    document.getElementById("contact-mail").setSelectionRange(0,0)
-    document.getElementById("toast").style.display=inline-block
+    document.getElementById("contact-mail").setSelectionRange(0, 0)
+    document.getElementById("toast").style.display = inline - block
 }
 
 // Show toast after selecting mail address in contact us
-$(document).ready(function(){
-    $("#copied").click(function(){
-        $('.toast').toast({delay: 2000});
-      $('.toast').toast('show');
+$(document).ready(function () {
+    $("#copied").click(function () {
+        $('.toast').toast({ delay: 2000 });
+        $('.toast').toast('show');
     });
-  });
+});
+
+
+//   Video
+function playPause() {
+    if (introvideo.paused)
+        introvideo.play();
+    else
+        introvideo.pause();
+} 
