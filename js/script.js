d230a07d677f8b15bb4233d3a43761f828d824f4
rhasan11 <AKSHAY@DESKTOP.com>
2020-09-17 17:46:06 +0530

    added carousel and sections

--- js/script.js
+++ js/script.js
@@ -2,4 +2,14 @@ function copyToClipboard() {
     document.getElementById("contact-mail").select()
     document.getElementById("contact-mail").setSelectionRange(0,99999)
     document.execCommand("copy")
+    document.getElementById("contact-mail").setSelectionRange(0,0)
+    document.getElementById("toast").style.display=inline-block
 }
+
+// Show toast after selecting mail address in contact us
+$(document).ready(function(){
+    $("#copied").click(function(){
+        $('.toast').toast({delay: 2000});
+      $('.toast').toast('show');
+    });
+  });
